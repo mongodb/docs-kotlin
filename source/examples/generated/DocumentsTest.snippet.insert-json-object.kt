edb4c79912e771d9642fd2c2895e7814b1ac89d3
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-04-26 11:44:40 -0400

    remove unused pages + first draft of Documents page with code examples

---
+++ source/examples/generated/DocumentsTest.snippet.insert-json-object.kt
@@ -0,0 +1,6 @@
+// MongoClient mongoClient = <code to instantiate your client>;
+
+val database = mongoClient.getDatabase("fundamentals_data")
+val collection= database.getCollection<JsonObject>("authors")
+
+val result = collection.insertOne(author)
