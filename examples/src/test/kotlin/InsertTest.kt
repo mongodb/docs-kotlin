35b16ff98949b77f2fcd93420a0c15629ce78250
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-04-27 10:16:15 -0400

    Apply initiial internal review feedback

--- examples/src/test/kotlin/InsertTest.kt
+++ examples/src/test/kotlin/InsertTest.kt
@@ -7,7 +7,6 @@ import com.mongodb.kotlin.client.coroutine.MongoClient
 import io.github.cdimascio.dotenv.dotenv
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
-import org.bson.BsonObjectId
 import org.bson.Document
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
@@ -17,31 +16,6 @@ import org.junit.jupiter.api.TestInstance
 import java.util.*
 import kotlin.test.*
 
-// :replace-start: {
-//    "terms": {
-//       "PaintOrder2": "PaintOrder",
-//       "paintOrder2": "paintOrder",
-//       "collection2": "collection"
-//    }
-// }
-
-// :snippet-start: retrieve-data-model
-
-data class PaintOrder(
-    @BsonId val id: Int,
-    val qty: Int,
-    val color: String
-)
-// :snippet-end:
-
-// :snippet-start: retrieve-data-model-2
-data class PaintOrder2(
-    @BsonId val id: ObjectId? = null,
-    val qty: Int,
-    val color: String
-)
-// :snippet-end:
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class InsertTest {
 
@@ -49,22 +23,28 @@ internal class InsertTest {
         val dotenv = dotenv()
         val client = MongoClient.create(dotenv["MONGODB_CONNECTION_URI"])
         val database = client.getDatabase("paint_store")
-        val collection = database.getCollection<PaintOrder>("paint_order")
-        val collection2 = database.getCollection<PaintOrder2>("paint_order2")
+
 
 
         @AfterAll
         @JvmStatic
         private fun afterAll() {
             runBlocking {
-                collection.drop()
-                collection2.drop()
                 client.close()
             }
         }
     }
     @Test
     fun insertManyErrorTest() = runBlocking {
+        // :snippet-start: retrieve-data-model
+
+        data class PaintOrder(
+            @BsonId val id: Int,
+            val qty: Int,
+            val color: String
+        )
+        // :snippet-end:
+        val collection = database.getCollection<PaintOrder>("paint_order")
         val paintOrders = listOf(
             PaintOrder(3,  5, "red"),
             PaintOrder(4, 10, "purple"),
@@ -75,12 +55,12 @@ internal class InsertTest {
         // :snippet-start: insert-many-error
         try {
             val result = collection.insertMany(paintOrders)
-            result.insertedIds.values
-                .forEach { doc -> doc.asInt32().value }
+            println(result.insertedIds.values
+                .forEach { it.asInt32().value })
             println("Inserted a document with the following ids: ${result.insertedIds}")
         } catch(e: MongoBulkWriteException){
             e.writeResult.inserts
-                .forEach { doc -> doc.id.asInt32().value }
+                .forEach { it.id.asInt32().value }
             println(
                 "A MongoBulkWriteException occurred, but there are " +
                 "successfully processed documents with the following ids: ${e.writeResult}"
@@ -91,19 +71,27 @@ internal class InsertTest {
         //Junit test for the above code
         val filter = Filters.empty()
         val testResults = listOf(Aggregates.match(filter))
-        collection.aggregate<Document>(testResults).toList().forEach { println(it.toJson()) }
         val expected = listOf(
             Document("_id", 3).append("qty", 5).append("color", "red"),
             Document("_id", 4).append("qty", 10).append("color", "purple")
         )
         assertEquals(expected, collection.aggregate<Document>(testResults).toList())
+        collection.drop()
     }
 
     @Test
     fun insertOneTest() = runBlocking {
+        // :snippet-start: retrieve-data-model-2
+        data class PaintOrder(
+            @BsonId val id: ObjectId? = null,
+            val qty: Int,
+            val color: String
+        )
+        // :snippet-end:
+        val collection = database.getCollection<PaintOrder>("paint_order")
         // :snippet-start: insert-one
-        val paintOrder = PaintOrder2(qty = 5, color = "red")
-        val result = collection2.insertOne(paintOrder)
+        val paintOrder = PaintOrder(qty = 5, color = "red")
+        val result = collection.insertOne(paintOrder)
 
         val insertedId = result.insertedId?.asObjectId()?.value
 
@@ -112,28 +100,35 @@ internal class InsertTest {
         // Junit test for the above code
         val filter = Filters.empty()
         val testResults = listOf(Aggregates.match(filter))
-        collection2.aggregate<Document>(testResults).toList().forEach { println(it.toJson()) }
         val expected = listOf(
             Document("_id", insertedId).append("qty", 5).append("color", "red")
         )
-        assertEquals(expected, collection2.aggregate<Document>(testResults).toList())
+        assertEquals(expected, collection.aggregate<Document>(testResults).toList())
+        collection.drop()
     }
 
     @Test
     fun insertManyTest() = runBlocking {
+        data class PaintOrder(
+            @BsonId val id: ObjectId? = null,
+            val qty: Int,
+            val color: String
+        )
+        val collection = database.getCollection<PaintOrder>("paint_order")
         // :snippet-start: insert-many
         val paintOrder2 = listOf(
-            PaintOrder2(qty = 5, color = "red"),
-            PaintOrder2(qty = 10, color = "purple"))
-        val result = collection2.insertMany(paintOrder2)
+            PaintOrder(qty = 5, color = "red"),
+            PaintOrder(qty = 10, color = "purple"))
+        val result = collection.insertMany(paintOrder2)
 
         result.insertedIds.values
-            .forEach { doc -> doc.asObjectId().value }
+         .forEach { it.asObjectId().value }
 
         println("Inserted a document with the following ids: ${result.insertedIds}")
         // :snippet-end:
         // Junit test for the above code
         assertTrue(result.wasAcknowledged())
+        collection.drop()
     }
 }
 // :replace-end:
