a5d1884f2f284a203fe4e7e37dd933acf4931e2f
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-04-28 13:59:10 -0400

    Delete unneeded snippet

--- source/examples/generated/LimitTest.snippet.insert-many.kt
+++
@@ -1,9 +0,0 @@
-val books = listOf(
-    Book(1, "The Brothers Karamazov", "Dostoyevsky", 824),
-    Book(2, "Les Misérables", "Hugo", 1462),
-    Book(3, "Atlas Shrugged", "Rand", 1088),
-    Book(4,"Infinite Jest", "Wallace", 1104),
-    Book(5, "Cryptonomicon", "Stephenson", 918),
-    Book(6, "A Dance with Dragons", "Martin", 1104)
-)
-collection.insertMany(books)
