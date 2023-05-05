cdb8e4e1871fb7d8d0001f09acddfc43ccf0c7ab
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-05-05 13:44:29 -0400

    implement JY feedback + add links

--- examples/src/test/kotlin/DocumentsTest.kt
+++ examples/src/test/kotlin/DocumentsTest.kt
@@ -155,13 +155,16 @@ internal class DocumentsTest {
     @Test
     fun jsonObjectTest() = runBlocking {
         // :snippet-start: create-json-object
-        val ejsonStr = ("{\"_id\": {\"\$oid\": \"6035210f35bd203721c3eab8\"},"
-                + "\"name\": \"Gabriel Garc\\u00eda M\\u00e1rquez\","
-                + "\"dateOfDeath\": {\"\$date\": \"2014-04-17T04:00:00Z\"},"
-                + "\"novels\": ["
-                + "{\"title\": \"One Hundred Years of Solitude\",\"yearPublished\": 1967},"
-                + "{\"title\": \"Chronicle of a Death Foretold\",\"yearPublished\": 1981},"
-                + "{\"title\": \"Love in the Time of Cholera\",\"yearPublished\": 1985}]}")
+        val ejsonStr = """
+            {"_id": {"${"$"}oid": "6035210f35bd203721c3eab8"},
+            "name": "Gabriel García Márquez",
+            "dateOfDeath": {"${"$"}date": "2014-04-17T04:00:00Z"},
+            "novels": [
+                {"title": "One Hundred Years of Solitude","yearPublished": 1967},
+                {"title": "Chronicle of a Death Foretold","yearPublished": 1981},
+                {"title": "Love in the Time of Cholera","yearPublished": 1985}]}
+            """.trimIndent()
+
         val author = JsonObject(ejsonStr)
         // :snippet-end:
 
