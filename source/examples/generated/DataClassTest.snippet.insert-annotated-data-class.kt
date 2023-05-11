b5d6f213ff33e4135685c1d2d6c5ae6cfccec26d
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-05-11 11:26:49 -0400

    draft data class page

---
+++ source/examples/generated/DataClassTest.snippet.insert-annotated-data-class.kt
@@ -0,0 +1,8 @@
+val collection = database.getCollection<NetworkDevice>(
+    "network_devices"
+)
+
+// Insert the record
+val deviceId = ObjectId().toHexString()
+val device = NetworkDevice(deviceId, "Enterprise Wi-fi", "router")
+collection.insertOne(device)
