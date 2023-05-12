7b2d2ccce73517c05e0f96f4be8ecc1a14de5110
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-05-12 10:35:13 -0400

    fully draft

---
+++ source/examples/generated/FlowTest.snippet.explain.kt
@@ -0,0 +1,7 @@
+val explanation = collection.find().explain(ExplainVerbosity.EXECUTION_STATS)
+val jsonSummary = explanation.getEmbedded(
+    listOf("queryPlanner", "winningPlan"),
+    Document::class.java
+)
+    .toJson()
+println(jsonSummary)
---
+++ source/examples/generated/FlowTest.snippet.firstOrNull.kt
@@ -0,0 +1,2 @@
+val resultsFlow = collection.find()
+val firstResult = resultsFlow.firstOrNull()
