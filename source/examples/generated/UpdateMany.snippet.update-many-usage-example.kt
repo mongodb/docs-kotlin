edd8b364a678ba762ce2d5cbbdd6ebab3fcf51fe
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-05-22 14:04:49 -0400

    Add Replace page

---
+++ source/examples/generated/UpdateMany.snippet.update-many-usage-example.kt
@@ -0,0 +1,35 @@
+
+import com.mongodb.MongoException
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.Updates
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import java.sql.Timestamp
+
+
+data class Movie(val num_mflix_comments: Int, val genres: List<String>)
+
+data class Results(val lastUpdated: Timestamp)
+
+fun main() = runBlocking {
+    // Replace the uri string with your MongoDB deployment's connection string
+    val uri = "<connection string uri>"
+    val mongoClient = MongoClient.create(uri)
+    val database = mongoClient.getDatabase("sample_mflix")
+    val collection = database.getCollection<Movie>("movies")
+
+    val query = Filters.gt(Movie::num_mflix_comments.name, 50)
+    val updates = Updates.combine(
+        Updates.addToSet(Movie::genres.name, "Frequently Discussed"),
+        Updates.currentTimestamp("${Results::lastUpdated.name}")
+    )
+    try {
+        val result = collection.updateMany(query, updates)
+        println("Modified document count: " + result.modifiedCount)
+    } catch (e: MongoException) {
+        System.err.println("Unable to update due to an error: $e")
+    }
+    println(collection.find(query).toList())
+    mongoClient.close()
+}
