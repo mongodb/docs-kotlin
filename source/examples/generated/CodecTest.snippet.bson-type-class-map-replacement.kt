e8bdf2a67fa037fc124068e3fd3e79d569b8fd2e
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-05-26 14:57:36 -0400

    copy corrections

--- source/examples/generated/CodecTest.snippet.bson-type-class-map-replacement.kt
+++ source/examples/generated/CodecTest.snippet.bson-type-class-map-replacement.kt
@@ -1,4 +1,5 @@
-val replacements = mutableMapOf<BsonType, Class<*>>(BsonType.ARRAY to MutableSet::class.java) // TODO: see if can find better typing for this than `Any`
+// TODO: see if can find better typing for this than `*`
+val replacements = mutableMapOf<BsonType, Class<*>>(BsonType.ARRAY to MutableSet::class.java)
 val bsonTypeClassMap = BsonTypeClassMap(replacements)
 val clazz = bsonTypeClassMap[BsonType.ARRAY]
 println("Java type: " + clazz.name)
--- source/examples/generated/CodecTest.snippet.codec-provider.kt
+++ source/examples/generated/CodecTest.snippet.codec-provider.kt
@@ -2,6 +2,6 @@ class MonolightCodecProvider : CodecProvider {
     override fun <T> get(clazz: Class<T>, registry: CodecRegistry): Codec<T>? {
         return if (clazz == Monolight::class.java) {
             MonolightCodec(registry) as Codec<T>
-        } else null // return null when not a provider for the requested class
+        } else null // Return null when not a provider for the requested class
     }
 }
