34a8242093bc74c8e22b17c8cc77bd997938d989
rustagir <rea.rustagi@mongodb.com>
2024-01-10 12:28:37 -0500

    JS PR fixes - remove projection stage

--- source/examples/generated/AggregatesBuilderTest.snippet.vector-search.kt
+++ source/examples/generated/AggregatesBuilderTest.snippet.vector-search.kt
@@ -1,11 +1,8 @@
-listOf(
-    Aggregates.vectorSearch(
-        SearchPath.fieldPath(MovieAlt::plotEmbedding.name),
-        listOf(-0.0072121937, -0.030757688, -0.012945653),
-        "mflix_movies_embedding_index",
-        2.toLong(),
-        1.toLong(),
-        vectorSearchOptions().filter(Filters.gte(MovieAlt::year.name, 2016))
-    ),
-    Aggregates.project(Projections.metaVectorSearchScore("vectorSearchScore"))
+Aggregates.vectorSearch(
+    SearchPath.fieldPath(MovieAlt::plotEmbedding.name),
+    listOf(-0.0072121937, -0.030757688, -0.012945653),
+    "mflix_movies_embedding_index",
+    2.toLong(),
+    1.toLong(),
+    vectorSearchOptions().filter(Filters.gte(MovieAlt::year.name, 2016))
 )
