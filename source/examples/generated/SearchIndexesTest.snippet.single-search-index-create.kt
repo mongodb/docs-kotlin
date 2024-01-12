fba81379c03cddd27dd3cbfdcbc190b546f605b6
rustagir <rea.rustagi@mongodb.com>
2024-01-12 13:38:45 -0500

    DOCSP-33429: atlas search idx mgmt

---
+++ source/examples/generated/SearchIndexesTest.snippet.single-search-index-create.kt
@@ -0,0 +1,6 @@
+val index = Document(
+    "mappings",
+    Document("dynamic", true)
+)
+val resultCreateIndex = moviesCollection.createSearchIndex("myIndex", index)
+println("Index created: $resultCreateIndex")
