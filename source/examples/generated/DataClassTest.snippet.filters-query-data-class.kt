82b8d6749f52e63828a7094af377bcd5c32f2239
rustagir <rea.rustagi@mongodb.com>
2025-01-24 10:50:40 -0500

    NH tech review 2

--- source/examples/generated/DataClassTest.snippet.filters-query-data-class.kt
+++
@@ -1,7 +0,0 @@
-val record = DataStorage("SSD", 120.0)
-// Infixed query
-DataStorage::productName.eq(record.productName)
-// Nested query
-val bson = eq(DataStorage::productName, record.productName)
-// Kmongo DSL
-val filter = DataStorage::productName eq record.productName
